7c666e04f472a06e147f94bc6995049461e30b52
Samay Gandhi <79245578+gandhisamay@users.noreply.github.com>
2021-06-13 12:40:58 +0530

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -1,10 +1,6 @@
-from torch.utils.data import Dataset,DataLoader
-from torchvision import transforms,datasets
-import torchvision.transforms.functional as TF
-
-color_scheme = pd.read_csv('class_dict_seg.csv')
-
-color_scheme
+images = 'dataset/semantic_drone_dataset/Original_Images'
+rgb_masks = 'RGB_color_image_masks'
+labels = 'dataset/semantic_drone_dataset/Labels'
 
 class DroneDataset(Dataset):
     def __init__(self,images_path,labels_path):
@@ -20,20 +16,23 @@ class DroneDataset(Dataset):
                                           transforms.ToTensor()
         ]))
 
-    def __getitem__(self,index): 
+    def __getitem__(self,index):
         img_output = self.labels[index][0]
         img_output = 255*img_output
+        #Manipulate the label Images
+        mask = np.array([[0,0],[1,1],[2,0],[3,3],[4,1],[5,5],[6,0],[7,5],[8,3],[9,2],[10,0],[11,2],[12,2],[13,0],[14,0],[15,5],[16,5],[17,6],[18,6],[19,3],
+                          [20,3],[21,0],[22,0],[23,0]])
+        for i in range(0,24):
+          img_output[img_output == i] = mask[i][1]
+
         img_output = img_output.to(torch.int64)
         return self.images[index][0],img_output
-  
+
     def __len__(self):
         return len(self.images)
 
 dataset = DroneDataset(images,labels)
 
-dataset[0][1].shape
-
-#Split the data into train and val dataset
 n_val = 10
 
 train_dataset,val_dataset = random_split(dataset,[len(dataset)-n_val,n_val],generator=torch.Generator().manual_seed(42))
